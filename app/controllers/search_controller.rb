60678f9345185756de5b68fe3f3408632a6e73f6
Wander Hillen <wjw.hillen@gmail.com>
2022-02-04 21:05:18 +0100

    Clean up show method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,8 +7,8 @@ class SearchController < ActionController::Base
   def show
     full_name = params.require(:fullname)
     entities = SanctionedEntity.where("full_name % :full_name", full_name: full_name).to_a
-    exact_match = @entities.map(&:full_name).any? {|entity_full_name| entity_full_name == full_name }
+    exact_match = entities.map(&:full_name).any? {|entity_full_name| entity_full_name == full_name }
 
-    render json: { exact_match: exact_match, results: @entities }
+    render json: { exact_match: exact_match, results: entities }
   end
 end
