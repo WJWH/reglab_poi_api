639fdb28688bcedb81e3a2bd3fed0fd18058a227
Wander Hillen <wjw.hillen@gmail.com>
2022-02-04 21:02:01 +0100

    Add http basic auth to SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
-class SearchController < ApplicationController
+class SearchController < ActionController::Base
+  include ActionController::HttpAuthentication::Basic
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+                               password: ENV["HTTP_BASIC_PASSWORD"],
+                               if: -> { ENV["HTTP_BASIC_USER"].present? && ENV["HTTP_BASIC_PASSWORD"].present? }
+
   def show
     full_name = params.require(:fullname)
     @entities = SanctionedEntity.where("full_name % :full_name", full_name: full_name).to_a
